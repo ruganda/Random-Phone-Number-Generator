9b75371df23f24544da3687f65857425a835028d
ruganda <mubaruganda@gmail.com>
2019-07-10 11:48:47 +0300

    initial project set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Random phone number generator </h1>
+    </div>
+  );
+}
+
+export default App;
